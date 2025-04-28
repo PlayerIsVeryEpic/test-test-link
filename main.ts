1d0ac0b328b28e573ad67ce040dc15d4ef16f1d9
Wolfy0906 <106831974+Wolfy0906@users.noreply.github.com>
2025-04-28 01:18:18 -0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,60 +1,40 @@
-input.onPinPressed(TouchPin.P0, function on_pin_pressed_p0() {
-    
-    if (time - globalTime > 0) {
-        score += 5
-    }
-    
-})
-//  insert sick beats here if u want
-radio.onReceivedNumber(function on_received_number(receivedNumber: number) {
-    
+// insert sick beats here if u want
+radio.onReceivedNumber(function (receivedNumber) {
     if (A_antiExplode == 0) {
         A_antiExplode += 1
     }
-    
-})
-input.onPinPressed(TouchPin.P1, function on_pin_pressed_p1() {
-    
-    if (time - globalTime > 0) {
-        score += 10
-    }
-    
 })
 let A_antiExplode = 0
-let globalTime = 0
-let time = 0
 radio.setGroup(143)
-//  set this to length of 1 attempt
+// set this to length of 1 attempt
 let ROUNDLENGTH = 10
-//  set this to break time between "A" press and game start
+// set this to break time between "A" press and game start
 let PAUSE = 10
-let highscore = 0
-time = 0
-let score = 0
-globalTime = 0
+let time = 0
+let globalTime = 0
 A_antiExplode = 0
-loops.everyInterval(1000, function on_every_interval() {
-    
+let score = 0
+let highscore = 0
+loops.everyInterval(1000, function () {
     globalTime += 1
 })
-basic.forever(function on_forever() {
-    
+basic.forever(function () {
     if (A_antiExplode == 1) {
         time = globalTime + PAUSE
         while (time > globalTime) {
-            radio.sendString("" + ("" + (time - globalTime)))
+            basic.showNumber(time - globalTime)
         }
-        //  This is clear screen command
-        radio.sendNumber(0)
-        radio.sendString("GO")
+        basic.clearScreen()
+        basic.showString("GO")
+        radio.sendValue("Start", 1)
         time = globalTime + ROUNDLENGTH
         while (time > globalTime) {
-            radio.sendString("" + ("" + (time - globalTime)))
+            basic.showNumber(time - globalTime)
         }
+        radio.sendValue("Start", 0)
         if (score > highscore) {
             highscore = score
         }
-        
         basic.showLeds(`
             # # # # #
             # # # # #
@@ -63,17 +43,16 @@ basic.forever(function on_forever() {
             # # # # #
             `)
         basic.pause(500)
-        radio.sendNumber(0)
-        radio.sendString("Score")
-        radio.sendString("" + ("" + score))
+        basic.clearScreen()
+        basic.showString("Score")
+        basic.showNumber(score)
         basic.pause(2000)
-        radio.sendNumber(0)
-        radio.sendString("HI")
-        radio.sendString("" + ("" + highscore))
+        basic.clearScreen()
+        basic.showString("HI")
+        basic.showNumber(highscore)
         basic.pause(2000)
-        radio.sendNumber(0)
+        basic.clearScreen()
         score = 0
         A_antiExplode = 0
     }
-    
 })
