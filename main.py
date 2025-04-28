d9d6cc25ea6ae51ee8519f7768b57095b30a81f3
Wolfy0906 <106831974+Wolfy0906@users.noreply.github.com>
2025-04-28 00:28:43 -0700

    Update pxt.json, main.blocks, main.ts, main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+def on_button_pressed_a():
+    radio.send_number(0)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_received_string(receivedString):
+    basic.show_string(receivedString)
+radio.on_received_string(on_received_string)
--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(0)
+})
 radio.onReceivedString(function (receivedString) {
     basic.showString(receivedString)
 })
+radio.setGroup(143)
