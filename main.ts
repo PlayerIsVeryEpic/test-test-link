a2a1b0ebf32daf63d1a5bb263dfabdc5a5da3a86
Wolfy0906 <106831974+Wolfy0906@users.noreply.github.com>
2025-04-27 21:50:02 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,3 @@
-radio.onReceivedValue(function (name, value) {
-    if (name == "a") {
-        basic.showString("SCORE")
-        basic.pause(500)
-        basic.clearScreen()
-        basic.showNumber(value)
-        basic.pause(500)
-    } else if (name == "b") {
-        basic.showString("HI")
-        basic.pause(500)
-        basic.clearScreen()
-        basic.showNumber(value)
-    } else if (name == "c") {
-        basic.showNumber(value)
-    } else {
-        basic.showString(name)
-    }
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
 })
